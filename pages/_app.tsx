3c806445dd8a23b3e001d42bbf3ad990907d769e
Julio Romero <julio_ale21@hotmail.com>
2021-04-05 20:36:35 -0300

    chore: initial UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,28 @@
 import React from "react"
-import { ChakraProvider } from "@chakra-ui/react"
-import { AppProps } from "next/app"
+import { ChakraProvider, VStack, Image, Heading, Text, Box, Divider } from "@chakra-ui/react"
+import { AppProps, Container } from "next/app"
+import theme from "theme"
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <Box padding={4}>
+        <Container 
+          backgroundColor="white"
+          boxShadow="md"
+          marginY={4}
+          maxWidth="container.xl"
+          padding={4}
+        >
+          <VStack marginY={6}>
+            <Image borderRadius={9999} src="//placehold.it/128x128"></Image>
+            <Heading>Ecommerce</Heading>
+            <Text>Mi Almacen</Text>
+          </VStack>
+          <Divider marginY={6} />
+          <Component {...pageProps} />
+        </Container>
+      </Box>
     </ChakraProvider>
   )
 }
