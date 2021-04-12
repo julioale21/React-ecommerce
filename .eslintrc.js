b82f5ce1db1c2bb780d40aaea97e9acc8bde3421
Julio Romero <julio_ale21@hotmail.com>
2021-04-11 21:39:55 -0300

    chore: changed rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -57,5 +57,6 @@ module.exports = {
       { blankLine: "always", prev: ["const", "let", "var"], next: "*" },
       { blankLine: "any", prev: ["const", "let", "var"], next: ["const", "let"] },
     ],
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
