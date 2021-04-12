6cc3210dbad3ae053f965095456363cc3fa11ac1
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-04-12 09:47:32 -0300

    chore: fixed test

--- utils/__test__/currency.test.ts
+++ utils/__test__/currency.test.ts
@@ -4,7 +4,7 @@ describe("currency", () => {
   describe("parseCurrency", () => {
     it("deberia devolver el precio localizado", () => {
       const actual = 65;
-      const expected = "ARS\xa065.00";
+      const expected = "$\xa065,00";
 
       expect(parseCurrency(actual)).toEqual(expected);
     });
