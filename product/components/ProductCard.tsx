8ea4fb5d67f7007a0af23bac3cb5f4c8bdfcb3ff
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-04-13 12:49:10 -0300

    chore: redesigned product card

--- product/components/ProductCard.tsx
+++ product/components/ProductCard.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Stack, Image, Text, Button, Center } from "@chakra-ui/react";
+import { Stack, Image, Text, Button } from "@chakra-ui/react";
 import { Product } from "product/types";
 import { parseCurrency } from "../../utils/currency";
 
@@ -15,34 +15,46 @@ const ProductCard: React.FC<Props> = ({ product, onAdd }) => {
       borderColor="gray.100"
       borderRadius="md"
       borderWidth={1}
-      boxShadow="md"
       data-testid="product"
+      direction="row"
+      justifyContent="space-between"
       padding={4}
       spacing={3}
     >
-      <Stack direction="row">
-        <Image
-          alt={product.title}
-          backgroundColor="white"
-          borderTopRadius="md"
-          height={16}
-          loading="lazy"
-          objectFit="contain"
-          src={product.image}
-          width={16}
-        />
-        <Stack spacing={1}>
-          <Text>{product.title}</Text>
-          <Text color="green.500" fontSize="md" fontWeight="500">
-            {parseCurrency(product.price)}
-          </Text>
+      <Stack direction="row" padding={4} width="100%">
+        <Stack justifyContent="space-between" spacing={1} width="100%">
+          <Stack spacing={1}>
+            <Text>{product.title}</Text>
+            <Text color="gray.500" fontSize="sm">
+              {product.description}
+            </Text>
+          </Stack>
+          <Stack alignItems="flex-end" direction="row" justifyContent="space-between">
+            <Text color="green.500" fontSize="md" fontWeight="500">
+              {parseCurrency(product.price)}
+            </Text>
+            <Button
+              colorScheme="primary"
+              size="sm"
+              variant="outline"
+              onClick={() => onAdd(product)}
+            >
+              Agregar
+            </Button>
+          </Stack>
         </Stack>
       </Stack>
-      <Center>
-        <Button colorScheme="primary" size="sm" variant="outline" onClick={() => onAdd(product)}>
-          Agregar
-        </Button>
-      </Center>
+
+      <Image
+        alt={product.title}
+        backgroundColor="white"
+        borderTopRadius="md"
+        height={36}
+        loading="lazy"
+        objectFit="contain"
+        src={product.image}
+        width={36}
+      />
     </Stack>
   );
 };
