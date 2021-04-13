8859155472aa7bb8bdf59cd962bba59ce62473ad
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-04-13 12:49:46 -0300

    chore: updated _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,5 @@
 import * as React from "react";
-import {
-  ChakraProvider,
-  VStack,
-  Image,
-  Heading,
-  Text,
-  Box,
-  Divider,
-  Container,
-} from "@chakra-ui/react";
+import { ChakraProvider, Stack, Image, Heading, Text, Divider, Container } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import theme from "theme";
 import { INFORMATION } from "../app/constants";
@@ -17,25 +8,23 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <ChakraProvider theme={theme}>
-        <Box padding={4}>
-          <Container
-            backgroundColor="white"
-            boxShadow="md"
-            marginY={4}
-            maxWidth="container.xl"
-            padding={4}
-          >
-            <VStack marginBottom={4}>
-              <Image borderRadius={9999} src={INFORMATION.avatar}></Image>
-              <Heading>{INFORMATION.title}</Heading>
-              <Text>{INFORMATION.description}</Text>
-            </VStack>
-            <Divider marginY={4} />
+        <Container backgroundColor="white" marginY={4} maxWidth="container.xl" padding={4}>
+          <Stack marginBottom={4} spacing={8}>
+            <Image borderRadius="lg" height="100%" maxHeight={64} src={INFORMATION.banner} />
+            <Stack alignItems="center" direction="row" spacing={6}>
+              <Image borderRadius={9999} height={32} src={INFORMATION.avatar} width={32} />
+              <Stack spacing={1}>
+                <Heading>{INFORMATION.title}</Heading>
+                <Text color="gray.500" fontWeight="500">
+                  {INFORMATION.description}
+                </Text>
+              </Stack>
+            </Stack>
             <Component {...pageProps} />
-            <Divider marginY={4} />
-            <Text textAlign="center">© Copyright {new Date().getFullYear()}</Text>
-          </Container>
-        </Box>
+          </Stack>
+          <Divider marginY={4} />
+          <Text textAlign="center">© Copyright {new Date().getFullYear()}</Text>
+        </Container>
       </ChakraProvider>
     </>
   );
